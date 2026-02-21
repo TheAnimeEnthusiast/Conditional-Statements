b2d7b404021b4f63a3764f7c30977e7a164e52f0
TheAnimeEnthusiast <mritvik02@gmail.com>
2026-02-20 20:27:42 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# write a program to check whether the given number is positive
+
+x = float(input("What number do you want to check?"))
+
+if x < 0:
+    print("Your number is negative")
+else:
+    print("Your number is positive")
